18c5055c7f5feb557270b2f2da32e4b3a4c1af8a
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2025-02-08 15:31:04 +0100

    ADD: Admin dashboard

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        
+
         return $this->render('profile/index.html.twig', [
             
         
