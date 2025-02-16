bc292f26935f9698f371c4889bf44fae734956a7
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2025-02-16 18:08:02 +0100

    ADD: details for a job

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\JobOfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,10 +18,10 @@ final class JobController extends AbstractController
     }
 
     #[Route('/job/{slug}', name: 'app_job_show')]
-    public function show(): Response
+    public function show(string $slug, JobOfferRepository $jobOfferRepository): Response
     {
         return $this->render('job/show.html.twig', [
-         
+            'jobOffer' => $jobOfferRepository->findOneBy(['slug' => $slug]),
         ]);
     }
 }
